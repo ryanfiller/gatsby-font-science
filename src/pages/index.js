df11d586798561e536a1e7657642b7906e0e5997
Ryan Filler <ryanfiller89@gmail.com>
2020-03-17 20:16:25 -0500

    add preload to header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 import './styles.scss'
+import Font from '../fonts/ScienceGothicVF.ttf'
 
 const IndexPage = () => (
+  <>
+   <Helmet>
+      <link
+        rel='preload'
+        as='font'
+        href={Font}
+        type='font/ttf'
+      />
+    </Helmet>
     <h1>
       <span>
         Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz
@@ -14,6 +25,7 @@ const IndexPage = () => (
         !@#$%&*_+-=?[]{}()
       </span>
     </h1>
+  </>
 )
 
 export default IndexPage
